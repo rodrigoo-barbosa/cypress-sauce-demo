191ed094a9262e219afb95b3036df9ff645726f0
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2025-05-15 17:32:47 -0400

    Removendo arquivo desnecessário

--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-// playwright.config.js
-const { defineConfig } = require('@playwright/test');
-
-module.exports = defineConfig({
-  use: {
-    headless: false,      
-    slowMo: 700           
-  },
-});
